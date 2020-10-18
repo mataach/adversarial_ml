78fa27a65867fb3fc0036fc1c7c20ac76460a076
Albert <49153611+mataach@users.noreply.github.com>
2020-10-18 17:05:04 +0200

    Train quantized ResNet18

---
+++ AAAI2021/Quantization/_quantUtils.py
@@ -0,0 +1,148 @@
+import os
+import time
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+from QuantResNet18 import *
+from QuantModules import *
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+use_cuda = True
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        
+        
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+def validate(val_loader, model, criterion):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    with torch.no_grad():
+        for i, (input, target) in enumerate(val_loader):
+            target = np.argmax(target, axis=1)
+
+            input, target = input.to(device), target.to(device)
+            
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            prec = accuracy(output, target)[0]
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec.item(), input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % 100 == 0:
+                print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec {top1.val:.3f}% ({top1.avg:.3f}%)'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1))
+
+    print(' * Prec {top1.avg:.3f}% '.format(top1=top1))
+
+    return top1.avg
+
+
+def train(trainloader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(trainloader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        # one-hot
+        target = np.argmax(target, axis=1)
+
+
+        input, target = input.to(device), target.to(device)
+
+
+        # compute output
+        output = model(input).to(device)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        prec = accuracy(output, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec.item(), input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # if i % 2 == 0:
+        #     model.module.show_params()
+        if i % 100 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec {top1.val:.3f}% ({top1.avg:.3f}%)'.format(
+                   epoch, i, len(trainloader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1))
+            
+def adjust_learning_rate(optimizer, epoch):
+    if epoch == 30:
+        return optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-04)
+    elif epoch == 40:
+        return optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-04)
+    return optimizer
+        
